b5bac4d60aa2696f983d1a5ac826063a8bba4624
Gianfranco Magliotti <gmagliotti@itba.edu.ar>
2025-03-29 03:47:34 -0300

    refactor: remove footer section from CustomLayout component for cleaner design

--- src/components/CustomLayout/CustomLayout.tsx
+++ src/components/CustomLayout/CustomLayout.tsx
@@ -8,20 +8,6 @@ const CustomLayout: React.FC<{ children: React.ReactNode }> = ({ children }) => 
       <div style={{ flex: 1, display: 'flex', flexDirection: 'column' }}>
         {children}
       </div>
-      {/* Footer */}
-        {/* <Divider style={{ margin: 0 }} />
-        <div style={{ textAlign: 'center', padding: 10, paddingInline: 20 }}>
-            <Flex justify='space-between'>
-                <div>
-                    <span>© {new Date().getFullYear()} Volantis</span>
-                </div>
-                <div>
-                    <Link to='/support'>Support</Link>
-                    <span> | </span>
-                    <Link to='/updates' target='_blank'>Changelog</Link>
-                </div>
-            </Flex>
-        </div> */}
     </div>
   );
 };
