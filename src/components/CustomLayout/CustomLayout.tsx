a23a6b99c854fbf3b796603b94fdb507215c35c0
Gianfranco Magliotti <gmagliotti@itba.edu.ar>
2025-03-29 04:14:17 -0300

    refactor: enhance layout structure with CustomLayout component and improve styling for LoginPage

--- src/components/CustomLayout/CustomLayout.tsx
+++ src/components/CustomLayout/CustomLayout.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import Navbar from '../Navbar/Navbar';
+import styles from './CustomLayout.module.css';
 
 const CustomLayout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   return (
-    <div style={{ display: 'flex', flexDirection: 'column', minHeight: '100vh' }}>
+    <div className={styles['custom-layout']}>
       <Navbar />
-      <div style={{ flex: 1, display: 'flex', flexDirection: 'column' }}>
+      <div className={styles['content']}>
         {children}
       </div>
     </div>
